c4ba0fea6f364a3fadeebd6b40112fa69cd96b4d
Kozou <kaz.0322.kei@gmail.com>
2021-12-08 15:59:29 +0900

    データベース更新

--- functions.php
+++ functions.php
@@ -1,5 +1,19 @@
 <?php
-                  
+//データベースに接続
+function db_open() :PDO{
+    $user = "root";
+    $password = "mariadb";
+    $opt = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::MYSQL_ATTR_MULTI_STATEMENTS => false
+    ];
+    $dbh = new PDO('mysql:host=localhost; dbname=kyuukyuu', $user, $password, $opt);
+    return $dbh;
+}
+
+//XSS対策
 function str(String $string) :string{
     return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
 }
+
